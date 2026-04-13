125d77529f63ebf8425b405bb1a313f109f12992
Adeyemi Adeniji <danieladeniji001@gmail.com>
2026-04-14 00:39:51 +0100

    Restored markdoc integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import vercel from '@astrojs/vercel';
 import keystatic from '@keystatic/astro';
 import react from '@astrojs/react';
+import markdoc from '@astrojs/markdoc'; // <-- Brought this back
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-	output: 'server',
-	adapter: vercel({
-		includeFiles: ['src/content/posts', 'src/content/site.json', 'public/uploads/posts'],
-	}),
-	integrations: [react(), keystatic()],
-  vite: {
-    plugins: [tailwindcss()]
-  }
+    output: 'server',
+    adapter: vercel({
+        includeFiles: ['src/content/posts', 'src/content/site.json', 'public/uploads/posts'],
+    }),
+    integrations: [react(), markdoc(), keystatic()], // <-- Plugged the engine back in
+    vite: {
+      plugins: [tailwindcss()]
+    }
 });
